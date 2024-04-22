dfd3ac6f732f4743e4acf9124bd95912aeb4f4be
abrahamayquipa <abrahamayquipa2000@gmail.com>
2024-04-21 21:55:42 -0500

    feat: add the ppages folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -13,11 +12,27 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './public/pages/home/home.component';
+import { HelpCenterComponent } from './public/pages/help-center/help-center.component';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { PaymentGatewayComponent } from './public/pages/payment-gateway/payment-gateway.component';
+import { ProductComponent } from './public/pages/product/product.component';
+import { SearcherComponent } from './public/pages/searcher/searcher.component';
+import { SettingsComponent } from './public/pages/settings/settings.component';
+import { ShoppingCartComponent } from './public/pages/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent,
+    HelpCenterComponent,
+    PageNotFoundComponent,
+    PaymentGatewayComponent,
+    ProductComponent,
+    SearcherComponent,
+    SettingsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
