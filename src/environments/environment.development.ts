7de61807d2674fb806930b89f926a12ff3e02c80
abrancitouu <abrahamayquipa2000@gmail.com>
2024-06-24 23:32:25 -0500

    feat: update the environments files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production:false,
-  serverBasePath: 'http://localhost:8080/api/v1',
+  serverBasePath: 'https://cartunnbackend.up.railway.app/api/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverBasePath: 'http://localhost:8080/api/v1',
+  serverBasePath: 'https://cartunnbackend.up.railway.app/api/v1',
 };
