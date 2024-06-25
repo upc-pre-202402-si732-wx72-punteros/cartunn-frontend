c54e9cd47ac97043bff1bb6fbfbac99afd56027a
AleRamosA <u202215164@upc.edu.pe>
2024-06-25 00:29:54 -0500

    feat: connect backend with order product button

--- src/app/products/components/products/products.component.ts
+++ src/app/products/components/products/products.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService} from "../../services/products-api/products.service";
 import { Product } from '../../model/product.entity';
 import { FavoritesApiService } from "../../../dashboard-and-analytics/services/favorites-api/favorites-api.service";
+import {NgForm} from "@angular/forms";
+import {Order} from "../../../orders/model/order.entity";
+import {OrdersService} from "../../../orders/services/orders.service";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-products',
@@ -10,13 +14,16 @@ import { FavoritesApiService } from "../../../dashboard-and-analytics/services/f
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
-
-  constructor(private productsService: ProductsService, private favoritesApiService: FavoritesApiService) {}
+  orders: Order[] = [];
+  order: Order = new Order();
+  lastOrder=0;
+  constructor(private productsService: ProductsService, private favoritesApiService: FavoritesApiService, private orderApiService: OrdersService) {}
 
   ngOnInit(): void {
     this.productsService.getProducts().subscribe((data: Product[]) => {
       this.products = data;
     });
+
   }
 
   toggleFavorite(product: Product): void {
@@ -26,5 +33,27 @@ export class ProductsComponent implements OnInit {
     } else {
       this.favoritesApiService.removeFavorite(product.id).subscribe();
     }
+  }
+  onOrderClick(product: Product): void {
+    this.orderApiService.getOrders().subscribe((data: any) => {
+      this.orders = data;
+      this.lastOrder=this.orders[this.orders.length-1].code;
+    });
+    this.order=new Order();
+    this.order.code=this.lastOrder+1;
+    this.order.name=product.title;
+    this.order.description=product.description;
+    this.order.status="In process";
+    var today = new Date();
+    //split
+    var dd = today.getDate();
+    var dd2 = today.getDate()+2;
+    var mm = today.getMonth() + 1; //because January is 0!
+    var yyyy = today.getFullYear();
+    this.order.entryDate=yyyy+'-'+(mm<10?'0'+mm:mm)+'-'+dd;
+    this.order.exitDate=yyyy+'-'+(mm<10?'0'+mm:mm)+'-'+dd2;
+    this.orderApiService.addOrder(this.order).subscribe();
+
+
   }
 }
