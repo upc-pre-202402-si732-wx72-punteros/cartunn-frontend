faf6ca5d3b3f625a02ca6fd6bb7a500df034afeb
AleRamosA <u202215164@upc.edu.pe>
2024-06-25 00:47:30 -0500

    feat: connect backend with order product button

--- src/app/dashboard-and-analytics/components/card/card.component.ts
+++ src/app/dashboard-and-analytics/components/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FavoritesApiService } from "../../services/favorites-api/favorites-api.service";
 import { Product } from "../../../products/model/product.entity";
+import {OrdersService} from "../../../orders/services/orders.service";
+import {Order} from "../../../orders/model/order.entity";
 
 @Component({
   selector: 'app-card-favorites',
@@ -9,8 +11,10 @@ import { Product } from "../../../products/model/product.entity";
 })
 export class FavoritesCardComponent {
   favorites: Product[] = [];
-
-  constructor(private favoritesApiService: FavoritesApiService) {}
+  orders: Order[] = [];
+  order: Order = new Order();
+  lastOrder=0;
+  constructor(private favoritesApiService: FavoritesApiService, private orderApiService: OrdersService) {}
 
 
   ngOnInit(): void {
@@ -30,5 +34,29 @@ export class FavoritesCardComponent {
       console.log(this.favorites);
       this.favorites = this.favorites.filter(product => product.id !== productId);
     });
+  }
+  onOrderClick(product: Product): void {
+    this.orderApiService.getOrders().subscribe((data: any) => {
+      this.orders = data;
+      if(this.orders.length>0) {
+        this.lastOrder = this.orders[this.orders.length - 1].code;
+      }
+    });
+    this.order=new Order();
+    this.order.code=this.lastOrder+1;
+    this.order.name=product.title;
+    this.order.description=product.description;
+    this.order.status="In process";
+    var today = new Date();
+    //split
+    var dd = today.getDate();
+    var dd2 = today.getDate()+2;
+    var mm = today.getMonth() + 1; //because January is 0!
+    var yyyy = today.getFullYear();
+    this.order.entryDate=yyyy+'-'+(mm<10?'0'+mm:mm)+'-'+dd;
+    this.order.exitDate=yyyy+'-'+(mm<10?'0'+mm:mm)+'-'+dd2;
+    this.orderApiService.addOrder(this.order).subscribe();
+
+
   }
 }
